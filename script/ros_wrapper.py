9e32a315dce110774a0bf21fe940afda025c70df
Prashant Ganesh <prashant.ganesh@ufl.edu>
2021-01-28 23:47:18 -0600

    making changes based on Kat_s feedback

--- script/ros_wrapper.py
+++ script/ros_wrapper.py
@@ -12,8 +12,12 @@ class WTAOptimization():
 
         self.num_weapons = rospy.get_param("num_weapons", 3)
         self.num_targets = rospy.get_param("num_targets", 2)
-        my_namespace = rospy.get_namespace()
+        self.k_max = rospy.get_param("max_iter", 100)
+        self.publishing_threshold = rospy.get_param("pub_prob", 0.2)
+        # my_namespace = rospy.get_namespace()
+        my_namespace = "robot01" # adding for  debugging
         self.my_number = int(my_namespace[-2])
+        print self.my_number
         self.my_primal_variable_idx = range(self.my_number * self.num_targets, (self.my_number+1) * self.num_targets)
         self.my_dual_variable_idx = self.my_number
         self.weapon_list = range(0, self.num_weapons)
@@ -40,8 +44,7 @@ class WTAOptimization():
         self.n = self.num_weapons * self.num_targets    #size of primal variable
         self.m = self.num_weapons                       #size of dual variable
         pBlocks = self.num_targets*np.arange(self.num_weapons)
-        
-        
+
         #initialize variables
         self.mu = np.zeros(self.m)  #shared in primal and dual updates
         self.x = .5*np.ones(self.n) #shared in primal and dual updates
@@ -53,18 +56,17 @@ class WTAOptimization():
         self.opt = DACOA(self.delta,self.gamma,self.rho, self.n, self.m)
         self.opt.defBlocks(pBlocks,np.arange(self.m))
         self.opt.useScalars()
-        # self.optimization()
-        
         self.a=self.opt.xBlocks[self.my_number]  #lower boundary of primal block (included)
         self.b=self.opt.xBlocks[self.my_number+1] #upper boundary of primal block (not included)
+        self.optimization()
 
     def primal_callback(self, msg, vehicle_num):
 
         if vehicle_num in self.weapon_list:
             self.weapon_list.pop(self.weapon_list.index(vehicle_num))
         elif not self.weapon_list:
-            self.update_dual_flag = True    #TODO: is this only set to true after an update is received from all agents?
-            self.weapon_list = range(0, self.num_weapons) +
+            self.update_dual_flag = True    #TODO: is this only set to true after an update is received from all agents? Yes
+            self.weapon_list = range(0, self.num_weapons)
             self.weapon_list.pop(self.my_number)
 
         self.stop_optimization = True
@@ -72,45 +74,45 @@ class WTAOptimization():
 
     def dual_callback(self, msg, vehicle_num):
         print("In dual callback", vehicle_num)
+        self.mu[vehicle_num] = msg.data
+        self.update_dual_flag = True
         self.weapon_list = range(0, self.num_weapons)
         self.weapon_list.pop(self.my_number)
-        self.mu[vehicle_num] = msg.data
-        # TODO: Update the dual variable
-        # TODO: If another dual variable is received, the update_dual_flag and counter (weapon_list?) need to be reset.
 
     def optimization(self):
 
         convdiff = [1]
         k = 0
-    
-        
-        while convdiff[k] > 10 ** -8 and not self.stop_optimization and not rospy.is_shutdown():
+        while convdiff[k] > 10 ** -8 and not self.stop_optimization and not rospy.is_shutdown() and k < self.k_max:
             if self.update_dual_flag:
                 self.update_duals()
                 self.update_dual_flag = False
             
             xUpdated = self.opt.singlePrimal(self.x, self.mu, self.my_number)
+            print xUpdated
             convdiff.append(la.norm(self.x,xUpdated))
             self.x = xUpdated
-            k +=1
-            # TODO for Prashant: How are we going to restart the optimization? Set a max k to get out of the loop
+            k += 1
 
-
-        if convdiff[k] < 10 ** -8: # Publish the primal agents a lot more frequently... Do it using a probabilty?
+        if np.random.normal(loc=0, scale=1) > self.publishing_threshold:
             pub_msg = Float32MultiArray()
-            pub_msg.data = self.x[a:b] # publishes just the block
+            pub_msg.data = self.x[self.my_primal_variable_idx] # publishes just the block
             self.primal_pub.publsh(pub_msg)
 
-            # Decide on how to get the assignments
+        self.stop_optimization = False
+        self.optimization() # calling this function recursively to make sure we keep getting assignments
+
+        # Decide on how to get the assignments
+        # if convdiff[k] < 10 ** -8: # if we have reached convergence, I think we should continue optimzing over and over to make sure we are assigned to the same target.
 
     def update_duals(self):
         
         muUpdated = self.opt.singleDual(self.x,self.mu[self.my_number],self.my_number)
         self.mu[self.my_number] = muUpdated
-
-        #Publish the Dual agents here
-
-
+        pub_msg = Float32MultiArray()
+        pub_msg.data = muUpdated
+        if np.random.normal(loc=0, scale=1) > self.publishing_threshold/3:
+            self.dual_pub.publish(pub_msg)
 
 
 if __name__ == '__main__':
